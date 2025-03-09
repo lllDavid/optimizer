e8b1a674f1c107d4154238507c65d9f6bb29f0f8
lllDavid <imperfectrising5527@gmail.com>
2025-03-09 17:10:15 +0100

    add directory exists check

--- benchmarks/profiler.py
+++ benchmarks/profiler.py
@@ -5,6 +5,18 @@ def profile_function(func, profile_type='before'):
     def wrapper(*args, **kwargs):
         base_directory = 'benchmarks/results'
         
+        if not path.exists(base_directory):
+            makedirs(base_directory)
+        
+        before_directory = path.join(base_directory, 'before')
+        optimized_directory = path.join(base_directory, 'optimized')
+
+        if not path.exists(before_directory):
+            makedirs(before_directory)
+        
+        if not path.exists(optimized_directory):
+            makedirs(optimized_directory)
+
         directory = path.join(base_directory, profile_type)
 
         if not path.exists(directory):
@@ -15,12 +27,12 @@ def profile_function(func, profile_type='before'):
 
         result = profiler(func)(*args, **kwargs)
 
-        filename = path.join(directory, f"{func.__name__}_line_stats.lprof")
+        filename = path.join(directory, f"{func.__name__}.lprof")
 
         with open(filename, 'w') as f:
             profiler.print_stats(stream=f)
 
         print(f"Finished profiling, saved file as '{filename}'.")
-        
+
         return result
     return wrapper
