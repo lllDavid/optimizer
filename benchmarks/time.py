46b23fb478b6b967b3145474c2504f92e7b03e56
lllDavid <imperfectrising5527@gmail.com>
2025-03-13 16:57:33 +0100

    replace time.time with perfcounter

--- benchmarks/time.py
+++ benchmarks/time.py
@@ -1,4 +1,4 @@
-import time
+from time import perf_counter
 from statistics import mean
 
 def time_function(repetitions=1):
@@ -6,9 +6,9 @@ def time_function(repetitions=1):
         def wrapper(*args, **kwargs):
             execution_times = []
             for _ in range(repetitions):
-                start_time = time.time()
+                start_time = perf_counter()
                 func(*args, **kwargs)
-                end_time = time.time()
+                end_time = perf_counter()
                 execution_times.append(end_time - start_time)
             avg_time = mean(execution_times)
             print(f"Avg. execution time over {repetitions} repetitions: {avg_time} seconds")
